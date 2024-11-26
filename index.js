d256da555a6454c011e260d749b6399b34c8a4b6
zuozuo_0203 <304160997@qq.com>
2024-11-26 17:33:03 +0800

    update

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ async function getGoldPrice() {
   );
   return result.data.result.dtList["1053"];
 }
-async function mail(messageInfo, goldInfo) {
+
+async function getWeather() {
+  const result = await axios.get(`http://api.k780.com/?app=weather.realtime&cityNm=${encodeURIComponent('长沙')}&ag=today&appkey=${nowapiConfig.AppKey}&sign=${nowapiConfig.Sign}&format=json`)
+  return result.data.result
+}
+
+async function getWeather2() {
+  const result = await axios.get(`http://aider.meizu.com/app/weather/listWeather?cityIds=101250106`)
+  return result.data.value
+}
+
+async function mail(messageInfo, goldInfo, weatherInfo) {
   let { minVal = -Infinity, maxVal = Infinity } = messageInfo;
   let { buy_price } = goldInfo;
   minVal = parseFloat(minVal);
@@ -30,17 +41,25 @@ async function mail(messageInfo, goldInfo) {
   }
   const mailOptions = {
     to: messageInfo.mail.replaceAll("、", ","), // 接收人列表,多人用','隔开
-    subject: "金价监控",
-    html: `<div>刘诗语你好呀<div>
-        <p>当前金价为${buy_price.toFixed(2)}</p>
+    subject: "今日特报！",
+    html: `<div style="width: 100%; height: 300px;display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; color: white; position: relative; padding: 20px; box-sizing: border-box;">
+    <h1 style="font-size: 36px; margin: 0;">今日特报</h1>
+    <div style="background: linear-gradient(to right, rgba(255, 0, 0, 0.7), rgba(255, 165, 0, 0.7)); padding: 20px; border-radius: 8px; max-width: 80%; z-index: 2;">
+        <p style="font-size: 20px; margin: 5px 0;">今天是${weatherInfo[0].weathers[0].week}</p>
+        <p style="font-size: 20px; margin: 5px 0;">天气：${weatherInfo[0].weathers[0].weather}</p>
+        <p style="font-size: 20px; margin: 5px 0;">最高温度为 ${weatherInfo[0].weathers[0].temp_night_c}℃</p>
+        <p style="font-size: 20px; margin: 5px 0;">最低温度为 ${weatherInfo[0].weathers[0].temp_day_c}℃</p>
+        <p style="font-size: 20px; margin: 5px 0;">当前金价为 ${buy_price} 元/g</p>
+    </div>
+       </div>
         `,
   };
   await sendMail(mailOptions);
 }
 const main = async () => {
-  const reqList = [getGoldPrice(), getTableDate()];
-  const [goldInfo, messageInfo] = await Promise.all(reqList);
-  await mail(messageInfo, goldInfo);
+  const reqList = [getGoldPrice(), getTableDate(), getWeather2()];
+  const [goldInfo, messageInfo, weatherInfo] = await Promise.all(reqList);
+  await mail(messageInfo, goldInfo, weatherInfo);
   process.exit(0);
 };
 
