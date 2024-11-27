5950ec2dd26ec0e28fea509c609a2b8c9d4de1a0
zuozuo_0203 <304160997@qq.com>
2024-11-27 14:33:11 +0800

    update

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 const PASS = process.env.PASS;
+const SEND_MAIL = process.env.SEND_MAIL
 const APP_KEY = process.env.APP_KEY;
 const SIGN = process.env.SIGN;
-const SEND_MAIL = process.env.SEND_MAIL
 
+
+console.log(SEND_MAIL)
 module.exports = {
   mail: {
     host: "smtp.qq.com", // 主机
     secureConnection: true, // 使用 SSL
     port: 465, // SMTP 端口
     auth: {
-      user: `${SEND_MAIL}`, // 自己用于发送邮件的账号
+      user: `304160997@qq.com`, // 自己用于发送邮件的账号
       pass: `${PASS}`, // 授权码(这个是假的,改成自己账号对应即可,获取方法: QQ邮箱-->设置-->账户-->POP3/IMAP/SMTP/Exchange/CardDAV/CalDAV服务-->IMAP/SMTP开启 复制授权码)
     },
   },
