7c6d7731f39860c7e6dafc5da835461fa1e8ea63
zuozuo_0203 <304160997@qq.com>
2024-11-27 11:45:46 +0800

    update

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ function time(futimg, now) {
   return "我们已经在一起：<br>" + day + "天了"
 }
 
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10);
+}
+
+const color = ['#23C343', '#0DA5AA', '#3491FA', '#FF7D00', '#E13EDB', '#F754A8', '#9FDB1D', '#FBE842', '#F77234', '#DDBEF6']
+
 async function mail(messageInfo, goldInfo, weatherInfo) {
   let { minVal = -Infinity, maxVal = Infinity } = messageInfo;
   let { buy_price } = goldInfo;
@@ -44,17 +51,42 @@ async function mail(messageInfo, goldInfo, weatherInfo) {
   if (minVal < buy_price && maxVal > buy_price) {
     return;
   }
+  let textLucky;
+  switch (weatherInfo[0].weathers[0].week) {
+    case '星期一':
+      textLucky = '愿你在新的一周里充满活力，迎接每一个挑战，开启美好的旅程！'
+      break;
+    case '星期二':
+      textLucky = '祝你今天心情愉快，工作顺利，愿每一个小目标都能实现！'
+      break;
+    case '星期三':
+      textLucky = '周中快乐！愿你在这一天找到灵感，继续向前迈进，收获满满！'
+      break;
+    case '星期四':
+      textLucky = '祝你今天充满正能量，克服困难，向着周末的美好生活努力！'
+      break;
+    case '星期五':
+      textLucky = '终于到周五了！愿你享受这一天的工作，期待一个愉快的周末！'
+      break;
+    case '星期六':
+      textLucky = '开开心心，放松心情'
+      break;
+    case '星期日':
+      textLucky = '乌拉拉，星期天啦，一周过完啦'
+      break;
+  }
   const mailOptions = {
     to: messageInfo.mail.replaceAll("、", ","), // 接收人列表,多人用','隔开
     subject: "今日小事-文",
     html: `<div style="width: 500px; height: 300px;display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center;position: relative; box-sizing: border-box;color: #555555;overflow: hidden;margin: 10px auto;padding: 15px 15px 15px 35px;border-radius: 10px;box-shadow: 6px 0 12px -5px rgb(253, 223, 234), -6px 0 12px -5px rgb(215, 240, 243);background-color: #FFDEE9;background-image: linear-gradient(0deg,#ffdee9c4 0%,#b5fffc8f 100%);background-image: -webkit-linear-gradient(0deg,#ffdee9c4 0%,#b5fffc8f 100%);">
-        <p style="font-size: 16px; margin: 5px 0;">今天: ${weatherInfo[0].weathers[0].week}</p>
-        <p style="font-size: 16px; margin: 5px 0;">${time('2024,1,27', weatherInfo[0].weathers[0].date)}</p>
-        <p style="font-size: 16px; margin: 5px 0;">天气：${weatherInfo[0].weathers[0].weather}</p>
-        <p style="font-size: 16px; margin: 5px 0;">最高温度: ${weatherInfo[0].weathers[0].temp_day_c}℃</p>
-        <p style="font-size: 16px; margin: 5px 0;">最低温度: ${weatherInfo[0].weathers[0].temp_night_c}℃</p>
-        <p style="font-size: 16px; margin: 5px 0;">${weatherInfo[0].indexes[2].content}</p>
-        <p style="font-size: 16px; margin: 5px 0;">当前金价: ${buy_price}元/g</p>
+        <p style="font-size: 16px; margin: 5px 0;">今天是<span style=color:${color[getRandomNumber()]}"> ${weatherInfo[0].weathers[0].week}</span></p>
+        <p style="font-size: 16px; margin: 5px 0;"><span style=color:${color[getRandomNumber()]}">${textLucky}</span></p>
+        <p style="font-size: 16px; margin: 5px 0;"><span style=color:${color[getRandomNumber()]}">${time('2024,1,27', weatherInfo[0].weathers[0].date)}</span></p>
+        <p style="font-size: 16px; margin: 5px 0;">天气：<span style=color:${color[getRandomNumber()]}">${weatherInfo[0].weathers[0].weather} </span></p>
+        <p style="font-size: 16px; margin: 5px 0;"><span style=color:${color[getRandomNumber()]}">${weatherInfo[0].indexes[2].content} </span></p>
+        <p style="font-size: 16px; margin: 5px 0;">最高温度: <span style=color:${color[getRandomNumber()]}">${weatherInfo[0].weathers[0].temp_day_c} </span>℃</p>
+        <p style="font-size: 16px; margin: 5px 0;">最低温度:<span style=color:${color[getRandomNumber()]}"> ${weatherInfo[0].weathers[0].temp_night_c} </span>℃</p>
+        <p style="font-size: 16px; margin: 5px 0;">当前金价:<span style=color:${color[getRandomNumber()]}">${buy_price}</span>元/g</p>
        </div>
         `,
   };
