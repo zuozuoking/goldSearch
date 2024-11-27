d9e643468cd19808c84966954724741a0354b7c3
zuozuo_0203 <304160997@qq.com>
2024-11-27 09:01:17 +0800

    update

--- index.js
+++ index.js
@@ -20,18 +20,13 @@ async function getGoldPrice() {
   return result.data.result.dtList["1053"];
 }
 
-async function getWeather() {
-  const result = await axios.get(`http://api.k780.com/?app=weather.realtime&cityNm=${encodeURIComponent('长沙')}&ag=today&appkey=${nowapiConfig.AppKey}&sign=${nowapiConfig.Sign}&format=json`)
-  return result.data.result
-}
-
 async function getWeather2() {
   const result = await axios.get(`http://aider.meizu.com/app/weather/listWeather?cityIds=101250106`)
   return result.data.value
 }
 
-function time(futimg) {
-  var nowtime = new Date().getTime(); // 现在时间转换为时间戳
+function time(futimg, now) {
+  var nowtime = new Date(now).getTime(); // 现在时间转换为时间戳
   var futruetime = new Date(futimg).getTime(); // 未来时间转换为时间戳
   var msec = nowtime - futruetime; // 毫秒 未来时间-现在时间
   var time = (msec / 1000);  // 毫秒/1000
@@ -56,7 +51,7 @@ async function mail(messageInfo, goldInfo, weatherInfo) {
         <h1 style="font-size: 36px; margin: 0;">今日特报</h1>
         <div style="background: linear-gradient(to right, rgba(255, 0, 0, 0.7), rgba(255, 165, 0, 0.7)); padding: 16px; border-radius: 8px; max-width: 80%; z-index: 2;">
         <p style="font-size: 16px; margin: 5px 0;">今天: ${weatherInfo[0].weathers[0].week}</p>
-        <p style="font-size: 16px; margin: 5px 0;">${time('2024,1,27')}</p>
+        <p style="font-size: 16px; margin: 5px 0;">${time('2024,1,27', weatherInfo[0].weathers[0].date)}</p>
         <p style="font-size: 16px; margin: 5px 0;">天气：${weatherInfo[0].weathers[0].weather}</p>
         <p style="font-size: 16px; margin: 5px 0;">最高温度: ${weatherInfo[0].weathers[0].temp_day_c}℃</p>
         <p style="font-size: 16px; margin: 5px 0;">最低温度: ${weatherInfo[0].weathers[0].temp_night_c}℃</p>
