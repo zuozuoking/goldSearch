ebdb383836e3cfb84cd6ab6d7381334fd4ec34e4
zuozuo_0203 <304160997@qq.com>
2024-11-27 08:51:07 +0800

    update

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function time(futimg) {
   var msec = nowtime - futruetime; // 毫秒 未来时间-现在时间
   var time = (msec / 1000);  // 毫秒/1000
   var day = parseInt(time / 86400); // 天  24*60*60*1000 
+  console.log(nowtime, 'nowtime')
   return "我们已经在一起：<br>" + day + "天了"
 }
 
@@ -51,16 +52,16 @@ async function mail(messageInfo, goldInfo, weatherInfo) {
   const mailOptions = {
     to: messageInfo.mail.replaceAll("、", ","), // 接收人列表,多人用','隔开
     subject: "今日特报！",
-    html: `<div style="width: 100%; height: 300px;display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; color: white; position: relative; padding: 20px; box-sizing: border-box;">
+    html: `<div style="width: 300px; height: 300px;display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; color: white; position: relative; padding: 16px; box-sizing: border-box;">
         <h1 style="font-size: 36px; margin: 0;">今日特报</h1>
-        <div style="background: linear-gradient(to right, rgba(255, 0, 0, 0.7), rgba(255, 165, 0, 0.7)); padding: 20px; border-radius: 8px; max-width: 80%; z-index: 2;">
-        <p style="font-size: 20px; margin: 5px 0;">今天: ${weatherInfo[0].weathers[0].week}</p>
-        <p style="font-size: 20px; margin: 5px 0;">${time('2024,1,27')}</p>
-        <p style="font-size: 20px; margin: 5px 0;">天气：${weatherInfo[0].weathers[0].weather}</p>
-        <p style="font-size: 20px; margin: 5px 0;">最高温度: ${weatherInfo[0].weathers[0].temp_day_c}℃</p>
-        <p style="font-size: 20px; margin: 5px 0;">最低温度: ${weatherInfo[0].weathers[0].temp_night_c}℃</p>
-        <p style="font-size: 20px; margin: 5px 0;">${weatherInfo[0].indexes[2].content}℃</p>
-        <p style="font-size: 20px; margin: 5px 0;">当前金价: ${buy_price}元/g</p>
+        <div style="background: linear-gradient(to right, rgba(255, 0, 0, 0.7), rgba(255, 165, 0, 0.7)); padding: 16px; border-radius: 8px; max-width: 80%; z-index: 2;">
+        <p style="font-size: 16px; margin: 5px 0;">今天: ${weatherInfo[0].weathers[0].week}</p>
+        <p style="font-size: 16px; margin: 5px 0;">${time('2024,1,27')}</p>
+        <p style="font-size: 16px; margin: 5px 0;">天气：${weatherInfo[0].weathers[0].weather}</p>
+        <p style="font-size: 16px; margin: 5px 0;">最高温度: ${weatherInfo[0].weathers[0].temp_day_c}℃</p>
+        <p style="font-size: 16px; margin: 5px 0;">最低温度: ${weatherInfo[0].weathers[0].temp_night_c}℃</p>
+        <p style="font-size: 16px; margin: 5px 0;">${weatherInfo[0].indexes[2].content}</p>
+        <p style="font-size: 16px; margin: 5px 0;">当前金价: ${buy_price}元/g</p>
       
     </div>
        </div>
