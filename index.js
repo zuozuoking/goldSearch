0c760d82e496ba9cd43c497109129ea939616ec5
zuozuo_0203 <304160997@qq.com>
2024-11-29 15:00:29 +0800

    update

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function mail(goldInfo, weatherInfo) {
   const html = `<div style="width: 500px; height: auto;display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center;position: relative; box-sizing: border-box;color: #555555;overflow: hidden;margin: 10px auto;padding: 15px 15px 15px 35px;border-radius: 10px;box-shadow: 6px 0 12px -5px rgb(253, 223, 234), -6px 0 12px -5px rgb(215, 240, 243);background-color: #FFDEE9;background-image: linear-gradient(0deg,#ffdee9c4 0%,#b5fffc8f 100%);background-image: -webkit-linear-gradient(0deg,#ffdee9c4 0%,#b5fffc8f 100%);">
   <p style="font-size: 16px; margin: 5px 0;">今天是<span style="color:${color[getRandomNumber()]}"> ${weatherInfo[0].weathers[0].week}</span></p>
   <p style="font-size: 16px; margin: 5px 0;"><span style="color:${color[getRandomNumber()]}">${textLucky}</span></p>
-  <p style="font-size: 16px; margin: 5px 0;"><span style="color:${color[getRandomNumber()]}">${time('2024,1,27', weatherInfo[0].weathers[0].date)}</span></p>
+  <p style="font-size: 16px; margin: 5px 0;"><span style="color:${color[getRandomNumber()]}">${time(process.env.FIRST_DAY, weatherInfo[0].weathers[0].date)}</span></p>
   ${birthdayHtml()}   
   <p style="font-size: 16px; margin: 5px 0;">天气：<span style="color:${color[getRandomNumber()]}">${weatherInfo[0].weathers[0].weather} </span></p>
   <p style="font-size: 16px; margin: 5px 0;"><span style="color:${color[getRandomNumber()]}">${weatherInfo[0].indexes[2].content} </span></p>
